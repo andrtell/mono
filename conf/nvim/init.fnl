ce85e1412eabad477803f685e1534e3fb8177a48
andrtell <116459057+andrtell@users.noreply.github.com.>
2025-01-09 21:54:01 +0100

    nvim; add fennel & init

---
+++ conf/nvim/init.fnl
@@ -0,0 +1,41 @@
+(set vim.g.mapleader " ")
+(set vim.g.maplocalleader ",")
+
+(fn file-exists? [path] (vim.uv.fs_stat path))
+
+(fn shell-error? [] (not (= vim.v.shell_error 0)))
+
+(fn die [error-message] (io.write error-message) (os.exit 1))
+
+(fn clone-lazy [target-dir]
+	(let [cmd ["git" 
+		   "clone" 
+		   "--filter=blob:none" 
+		   "--branch=stable" 
+		   "https://github.com/folke/lazy.nvim.git" 
+		   target-dir]]
+	  (vim.fn.system cmd)))
+
+(fn clone-lazy-or-die [target-dir]
+	(let [out (clone-lazy target-dir)
+	      msg (out:gsub "%s+" " ")]
+	  (if (shell-error?) (die msg))))
+ 
+(fn install-lazy [packages-dir] 
+  (let [lazy-dir (.. packages-dir "/lazy.nvim")] 
+    (if (not (file-exists? lazy-dir))
+	(clone-lazy-or-die lazy-dir)) 
+    (vim.opt.rtp:prepend lazy-dir)))
+
+(local packages-dir (.. (vim.fn.stdpath :config) "/packages"))
+
+(install-lazy packages-dir)
+
+(local config {:root packages-dir
+       	       :lockfile (.. packages-dir "/lazy-lock.json")
+	       :spec [{ 1 "Olical/nfnl" :ft "fennel" }]
+		      ; {:import "spec"}]
+	       :checker {:enabled true }
+	       :install {:colorscheme [ "default" ] }})
+
+(let [lazy (require :lazy)] (lazy.setup config))
---
+++ conf/nvim/init.lua
@@ -0,0 +1,39 @@
+-- [nfnl] Compiled from init.fnl by https://github.com/Olical/nfnl, do not edit.
+vim.g.mapleader = " "
+vim.g.maplocalleader = ","
+local function file_exists_3f(path)
+  return vim.uv.fs_stat(path)
+end
+local function shell_error_3f()
+  return not (vim.v.shell_error == 0)
+end
+local function die(error_message)
+  io.write(error_message)
+  return os.exit(1)
+end
+local function clone_lazy(target_dir)
+  local cmd = {"git", "clone", "--filter=blob:none", "--branch=stable", "https://github.com/folke/lazy.nvim.git", target_dir}
+  return vim.fn.system(cmd)
+end
+local function clone_lazy_or_die(target_dir)
+  local out = clone_lazy(target_dir)
+  local msg = out:gsub("%s+", " ")
+  if shell_error_3f() then
+    return die(msg)
+  else
+    return nil
+  end
+end
+local function install_lazy(packages_dir)
+  local lazy_dir = (packages_dir .. "/lazy.nvim")
+  if not file_exists_3f(lazy_dir) then
+    clone_lazy_or_die(lazy_dir)
+  else
+  end
+  return vim.opt.rtp:prepend(lazy_dir)
+end
+local packages_dir = (vim.fn.stdpath("config") .. "/packages")
+install_lazy(packages_dir)
+local config = {root = packages_dir, lockfile = (packages_dir .. "/lazy-lock.json"), spec = {{"Olical/nfnl", ft = "fennel"}}, checker = {enabled = true}, install = {colorscheme = {"default"}}}
+local lazy = require("lazy")
+return lazy.setup(config)
