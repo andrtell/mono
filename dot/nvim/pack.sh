f7393a9fecc9241e02cdeb24f0e011f36d70fa47
andrtell <116459057+andrtell@users.noreply.github.com.>
2025-02-01 04:19:38 +0100

    nvim; update pack.sh

--- dot/nvim/pack.sh
+++ dot/nvim/pack.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 
-mkdir -p pack/vendor/start
+ROOT="pack/vendor/start"
+
+mkdir -p $ROOT
+
+cd $ROOT
+
+DIR="leap.nvim"		git -C "$DIR" pull || git clone https://github.com/ggandor/leap.nvim.git "$DIR"
+DIR="parinfer-rust"	git -C "$DIR" pull || git clone https://github.com/eraserhd/parinfer-rust.git "$DIR"
+DIR="vim-sayonara"	git -C "$DIR" pull || git clone https://github.com/mhinz/vim-sayonara.git "$DIR"
+DIR="tangerine.nvim"	git -C "$DIR" pull || git clone https://github.com/udayvir-singh/tangerine.nvim.git "$DIR"
