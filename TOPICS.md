3a728cda739f972c87cd0081b790065123c0c76e
andrtell <116459057+andrtell@users.noreply.github.com.>
2025-05-05 19:26:01 +0200

    wip

---
+++ TOPICS.md
@@ -0,0 +1,13 @@
+# C
+
+compiler: gcc
+debugger: gdb
+memory-test: valgrind
+profiler: gprof
+build: make
+libraries: pkg-config; autotools
+
+# Scheme / Guile
+
+
+# Guix
--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -4,8 +4,8 @@
 -- ╝╚╝╚═╝╚═╝ ╚╝ ╩╩ ╩
 ------------------------------------------------------------                 
 
-g = vim.g
-o = vim.opt
+local g = vim.g
+local o = vim.opt
 
 ------------------------------------------------------------
 -- OPTION
@@ -36,8 +36,8 @@ end)
 -- KEY
 ------------------------------------------------------------
 
-i = function(l, r, opts) vim.keymap.set("i", l, r, opts or {}) end
-n = function(l, r, opts) vim.keymap.set("n", l, r, opts or {}) end
+local i = function(l, r, opts) vim.keymap.set("i", l, r, opts or {}) end
+local n = function(l, r, opts) vim.keymap.set("n", l, r, opts or {}) end
 
 g.mapleader = " "
 g.maplocalleader = ","
@@ -66,11 +66,11 @@ n("S", "<Plug>(leap-from-window)")
 -- HELPERS
 ------------------------------------------------------------
 
-create_autocmd = vim.api.nvim_create_autocmd
-clear_autocmds = vim.api.nvim_clear_autocmds
-create_augroup = vim.api.nvim_create_augroup
+local create_autocmd = vim.api.nvim_create_autocmd
+local clear_autocmds = vim.api.nvim_clear_autocmds
+local create_augroup = vim.api.nvim_create_augroup
 
-create_buf_augroup=function(name, buf) 
+local create_buf_augroup=function(name, buf) 
     local group=create_augroup(name, {clear=false}) 
     clear_autocmds({group=group, buffer=buf})
     return group
@@ -84,7 +84,7 @@ g.netrw_banner = 0
 g.netrw_keepdir = 0
 g.netrw_list_hide="\\(^\\|\\s\\s\\)\\zs\\.\\S\\+"
 
-netrw_keys = function () 
+local netrw_keys = function () 
   local opt = { silent=true, buffer=true, remap=true }
   n("<esc>", ":Sayonara!<cr>", opt)
   n("h", "-", opt)
@@ -93,7 +93,7 @@ netrw_keys = function ()
   n("H", "h", opt)
 end
 
-netrw_group = create_augroup("netrw-0", {clear=true})
+local netrw_group = create_augroup("netrw-0", {clear=true})
 
 create_autocmd("filetype", {
   pattern="netrw",
@@ -112,62 +112,61 @@ vim.fn.sign_define("DiagnosticSignError", {text=""})
 
 vim.diagnostic.config {virtual_text=false}
 
-patch_diagnostic_underline = function () 
+local patch_underline = function () 
   local ns = vim.api.nvim_create_namespace "diagnostic"
-  local show0 = vim.diagnostic.handlers.underline.show
-  local show1 = function (_ns, buf, ds, opt)
+  local old_show = vim.diagnostic.handlers.underline.show
+  local old_hide = vim.diagnostic.handlers.underline.hide
+  local new_show = function (_ns, buf, ds, opt)
     for _, d in ipairs(ds) do
       if d.col == d.end_col then
         local last_col = vim.fn.strlen(vim.fn.getline("."))
         if d.col == last_col then
           d.col = 0
+        else
           d.end_col = last_col
         end
-        show0(ns, buf, ds, opt)
       end
     end
+    return old_show(ns, buf, ds, opt)
   end
-  local hide0 = vim.diagnostic.handlers.underline.hide
-  local hide1 = function ()
-    hide0(ns, buf) 
+  local new_hide = function (_ns, buf)
+    return old_hide(ns, buf) 
   end
-  vim.diagnostic.handlers.underline = {show=show1, hide=hide1}
+  vim.diagnostic.handlers.underline = {show=new_show, hide=new_hide}
 end
 
-patch_diagnostic_open_float = function ()
-  local open_float0 = vim.diagnostic.open_float 
-  local open_float1 = function(_opts) 
-    local opts = {
+local patch_open_float = function ()
+  local old_open_float = vim.diagnostic.open_float
+  local new_open_float = function(old_opts) 
+    local ext_opts = {
       border = {"┌", "─", "┐", "│", "┘", "─", "└", "│"},
-      focus = false,
-      header = "" ,
-      prefix = " ",
-      scope = "line",
-      suffix = " "
+      focus = false, header = "", prefix = " ", scope = "line", suffix = " "
     }
-    local bufnr, win_id = open_float0(opts)
+    local new_opts = vim.tbl_extend("force", old_opts or {}, ext_opts)
+    bufno, win_id = old_open_float(new_opts)
     if win_id then
-      local conf0 = vim.api.nvim_win_get_config(win_id)
-      local patch = {
+      local old_conf = vim.api.nvim_win_get_config(win_id)
+      local ext_conf = {
         relative = "win",
         win = vim.api.nvim_get_current_win(),
         col = 999,
         row = 0,
       }
-      local conf1 = vim.tbl_extend("force", conf0, patch)
-      vim.api.nvim_win_set_config(win, conf1)
+      local new_conf = vim.tbl_extend("force", old_conf, ext_conf)
+      vim.api.nvim_win_set_config(win_id, new_conf)
     end
+    return bufno, win_id
   end
-  vim.diagnostic.open_float = open_float1
+  vim.diagnostic.open_float = new_open_float
 end
 
-diagnostic_group = create_augroup("DIAGNOSTIC-0", {clear=true}) 
+local diagnostic_group = create_augroup("DIAGNOSTIC-0", {clear=true}) 
 
 create_autocmd("VimEnter", {
   group=diagnostic_group,
   callback=function ()
-    patch_diagnostic_underline()
-    patch_diagnostic_open_float()
+    patch_underline()
+    patch_open_float()
   end
 })
 
@@ -175,35 +174,37 @@ create_autocmd("VimEnter", {
 -- LSP
 ------------------------------------------------------------
 
-lsp_keys = function()
+local lsp_keys = function()
     local opt = { silent=true, buffer=true }
     n("gD", function () vim.lsp.buf.declaration() end)
     n("gd", function () vim.lsp.buf.definition() end)
     return false
 end
 
-lsp_format_buffer = function()
+local lsp_format_buffer = function()
     if vim.bo.modified then
-      vim.lsp.buf_format()
+      vim.lsp.buf.format()
     end
 end
 
-lsp_setup_format_buffer = function(buf)
+local lsp_setup_format_buffer = function(buf)
   local group = create_buf_augroup("LSP-1", buf)
-  create_autocmd("bufprewrite", {
+  create_autocmd("bufwrite", {
     buffer = buf,
     group = group,
-    callback = lsp_format_buffer,
+    callback = function()
+      lsp_format_buffer()
+    end
   })
 end
 
-lsp_organize_imports_buffer = function(buf)
+local lsp_organize_imports_buffer = function(buf)
   if not vim.bo.modified then
     return
   end
   local params0 = vim.lsp.util.make_range_params()
   local patch = {context={diagnostic={}, only={"source.organizeImports"}}}
-  local params1 = vim.tbl_extend("force", params0, patch) 
+  local params1 = vim.tbl_extend("force", params0 or {}, patch) 
   local timeout_ms = 500
   local response = vim.lsp.buf_request_sync(
     buf, 
@@ -218,23 +219,59 @@ lsp_organize_imports_buffer = function(buf)
   end
 end
 
-lsp_setup_organize_imports_buffer = function(buf)
+local lsp_setup_organize_imports_buffer = function(buf)
   local group = create_buf_augroup("LSP-2", buf)
-  create_autocmd("bufprewrite", {
+  create_autocmd("bufwrite", {
     buffer = buf,
     group = group,
-    callback = lsp_organize_imports_buffer
+    callback = function(ev)
+      lsp_organize_imports_buffer(ev.buf)
+    end
   })
 end
 
-lsp_group = create_augroup("LSP-0", {clear=true})
+local lsp_setup_show_float = function(buf)
+  local group = create_buf_augroup("LSP-3", buf)
+  create_autocmd("cursorhold", {
+    buffer = buf,
+    group = group,
+    callback = function(ev)
+      vim.diagnostic.open_float()
+      return false
+    end
+  })
+end
+
+local lsp_group = create_augroup("LSP-0", {clear=true})
 
 create_autocmd("lspattach", {
   group=lsp_group,
-  callback=function (ev) 
+  callback = function(ev) 
     lsp_keys()
     lsp_setup_format_buffer(ev.buf)
     lsp_setup_organize_imports_buffer(ev.buf)
+    lsp_setup_show_float(ev.buf)
+  end
+})
+
+local patch_open_floating_preview = function()
+  old_open_floating_preview = vim.lsp.util.open_floating_preview
+  new_open_floating_preview = function(contents, syntax, opts0, ...) 
+    patch = {
+      border = {"┌","─", "┐", "│", "┘", "─", "└", "│"},
+      max_width = 100,
+      focusable=true
+    }
+    opts1 = vim.tbl_extend("force", opts0 or {}, patch)
+    return old_open_floating_preview(contents, syntax, opts1, unpack(arg))
+  end
+  vim.lsp.util.open_floating_preview=new_open_floating_preview
+end
+
+create_autocmd("VimEnter", {
+  group=lsp_group,
+  callback=function ()
+    patch_open_floating_preview()
   end
 })
 
@@ -242,7 +279,7 @@ create_autocmd("lspattach", {
 -- GOLANG
 ------------------------------------------------------------
 
-golang_group = create_augroup("GOLANG-0", {clear=true})
+local golang_group = create_augroup("GOLANG-0", {clear=true})
 
 create_autocmd("filetype", {
   pattern={"go", "gomod", "gowork", "gotmpl"},
@@ -274,11 +311,11 @@ require('nvim-treesitter.configs').setup {
 -- COLOR
 ------------------------------------------------------------
 
-hl = function (group, opts)
+local hl = function (group, opts)
   vim.api.nvim_set_hl(0, group, opts)
 end
 
-hl_clear = function() 
+local hl_clear = function() 
   for hlgroup, _ in pairs(vim.api.nvim_get_hl(0, {})) do
     if type(hlgroup) == "string" then
       hl(hlgroup, {fg="#101010", bg="#fefefe"})
@@ -286,7 +323,7 @@ hl_clear = function()
   end
 end
 
-hl_set = function ()
+local hl_set = function ()
   local red = "#B20000"
   local green = "#188120"
   local blue = "#001097"
@@ -302,14 +339,13 @@ hl_set = function ()
   hl("pmenusel",      {bg="#dbdbdb"})
   hl("search",        {bg="#faf2d8"})
   hl("statusline",    {bg="#ebebeb"}) 
-  hl("visual",        {bg="#eff0f1"})
-  hl("visualnos",     {bg="#e3eefd"})
+  hl("visual",        {bg="#e4effe"})
+  hl("visualnos",     {bg="#e4effe"})
   hl("winseparator",  {fg="#e2e2e2"})
   -- code
   hl("@comment",      {fg="#9c9ea3"})
-  hl("@string",      {fg=green})
   -- lua
-  -- hl("@string.lua",              {fg="green"})
+  hl("@string.lua",              {fg=green})
   hl("@string.escape.lua",       {fg=green})
   hl("@boolean.lua",             {fg="purple"})
   hl("@number.lua",              {fg=red})
@@ -320,6 +356,7 @@ hl_set = function ()
   hl("@keyword.repeat.lua",      {fg=blue})
   hl("@keyword.return.lua",      {fg=blue})
   hl("@constructor.lua",         {fg="#3c3c45"})
+  -- go
   -- leap
   hl("LeapLabelPrimary",         {bg="#fadffa"})
   -- diagnostic
@@ -334,7 +371,7 @@ end
 
 -- hl_set()
 
-hl_augroup = create_augroup("HL", {clear=true})
+local hl_augroup = create_augroup("HL", {clear=true})
 
 create_autocmd("colorscheme", {
   group=hl_augroup,
